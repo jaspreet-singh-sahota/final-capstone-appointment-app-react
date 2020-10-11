d8c730ec63363e11cabf2da14485c147b1d0fe19
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-11 16:01:32 +0530

    Add input fields to log in page

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -12,7 +12,46 @@ const LogInPage = () => {
   const [errors, setErrors] = useState([])
   const dispatch = useDispatch()
 
-  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    
+
+  return (
+    <div>
+      {!user ?
+        <>
+          {
+            errors.length ? <ul>Error
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+            </ul> : null
+          }
+          < form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              value={username}
+              onChange={event => setUsername(event.target.value)}
+              required
+            />
+
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+              required
+            />
+
+            <button type="submit">Log in</button>
+          </form>
+        </>
+        : <Redirect to='/' />
+      }
+    </div >
+  );
 }
 
 export default LogInPage
