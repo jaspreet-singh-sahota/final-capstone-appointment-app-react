256a9fdd1b51765d7868a920c8c77ebb3ad83eeb
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-11 15:34:30 +0530

    Add axios request end-points

---
+++ src/axios/request.js
@@ -0,0 +1,8 @@
+const request = {
+  checkUserLoggedIn: 'http://localhost:8080/logged_in',
+  logUserIn: 'http://localhost:8080/sessions',
+  logUserOut: 'http://localhost:8080/logout',
+  SignUserIn: 'http://localhost:8080/registrations'
+}
+
+export default request
