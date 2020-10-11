04b7c78c851f36e2e397360e0926033d1bfa64e4
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-11 16:16:24 +0530

    Add input field to sign in page

--- src/pages/sign-in/SignInPage.js
+++ src/pages/sign-in/SignInPage.js
@@ -6,7 +6,57 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Redirect } from 'react-router'
 
 const SignInPage = () => {
-  
+ 
+
+  return (
+    <div>
+      {!user ?
+        <>
+          {errors.length ? <ul>Error
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+          </ul> : null}
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              value={username}
+              onChange={event => setUsername(event.target.value)}
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+              required
+            />
+
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+              required
+            />
+
+            <input
+              type="password"
+              name="password_confirmation"
+              placeholder="Password confirmation"
+              value={passwordConfirmation}
+              onChange={event => setPasswordConfirmation(event.target.value)}
+              required
+            />
+
+            <button type="submit">Register</button>
+          </form>
+        </>
+        : <Redirect to='/' />
+      }
+    </div>
   );
 }
 
