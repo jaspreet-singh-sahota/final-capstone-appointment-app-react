a64dcd8901c2b3a902b56be0adee3930a943b644
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-11 16:33:24 +0530

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Axios from 'axios';
+import { useDispatch } from 'react-redux';
+import logIn from './redux/actions/user-action/userAction';
+import request from './axios/request';
+import LogInPage from './pages/log-in/LogInPage';
+import SignInPage from './pages/sign-in/SignInPage';
+import homePage from './pages/home/homePage';
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>Testing</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={homePage} />
+        <Route exact path="/login" component={LogInPage} />
+        <Route exact path="/sign-in" component={SignInPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
