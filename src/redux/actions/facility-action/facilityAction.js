214cdb1f53a00fd7e716ddb7e7fd7e9c8ec9cd32
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-12 07:24:57 +0530

    Add facility fetching actions

---
+++ src/redux/actions/facility-action/facilityAction.js
@@ -0,0 +1,17 @@
+import { FETCH_FACILITY_REQUEST, FETCH_FACILITY_SUCCESS, FETCH_FACILITY_FAILURE } from "../actionType";
+
+
+export const fetchFacilityRequest = () => ({
+  type: FETCH_FACILITY_REQUEST,
+});
+
+export const fetchFacilitySuccess = (movieCollection, genre) => ({
+  type: FETCH_FACILITY_SUCCESS,
+  payload: movieCollection,
+  genre,
+});
+
+export const fetchFacilityFailure = error => ({
+  type: FETCH_FACILITY_FAILURE,
+  payload: error,
+});
