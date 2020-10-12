3982a886b20d6c673f69d10a68c8ae11798f6309
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-12 07:17:59 +0530

    Add facility data url to axios-request

--- src/axios/request.js
+++ src/axios/request.js
@@ -2,7 +2,8 @@ const request = {
   checkUserLoggedIn: 'http://localhost:8080/logged_in',
   logUserIn: 'http://localhost:8080/sessions',
   logUserOut: 'http://localhost:8080/logout',
-  SignUserIn: 'http://localhost:8080/registrations'
+  SignUserIn: 'http://localhost:8080/registrations',
+  facilityData: 'http://localhost:8080/facilities'
 }
 
 export default request
