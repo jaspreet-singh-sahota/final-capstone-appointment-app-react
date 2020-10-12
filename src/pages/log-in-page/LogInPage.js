eb1a3a4bc40ba094de5b5767141e26b3fdf8e719
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-12 07:22:55 +0530

    Add routes to login page

---
+++ src/pages/log-in-page/LogInPage.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import request from '../../axios/request'
+import Axios from 'axios'
+import logIn from '../../redux/actions/user-action/userAction'
+import { Redirect } from 'react-router'
+
+const LogInPage = () => {
+  const user = useSelector(state => state.user.isLoggedIn)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState([])
+  const dispatch = useDispatch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    Axios.post(
+      request.logUserIn,
+      {
+        user: {
+          username: username,
+          password: password,
+        }
+      },
+      { withCredentials: true })
+      .then(response => {
+        if (response.data.status === "created") {
+          dispatch(logIn(response.data.user.username))
+        } else {
+          setErrors([response.data.errors])
+        }
+      })
+      .catch(error => {
+        console.log("registration error", error);
+      });
+  }
+
+  return (
+    <div>
+      {!user ?
+        <>
+          {
+            errors.length ? <ul>Error
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+            </ul> : null
+          }
+          < form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              value={username}
+              onChange={event => setUsername(event.target.value)}
+              required
+            />
+
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+              required
+            />
+
+            <button type="submit">Log in</button>
+          </form>
+        </>
+        : <Redirect to='/' />
+      }
+    </div >
+  );
+}
+
+export default LogInPage
