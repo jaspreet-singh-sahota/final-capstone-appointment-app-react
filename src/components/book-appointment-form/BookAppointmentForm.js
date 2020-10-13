d3ca8e7b1bf9b57a9b5a53926a4faf82e7abeb9d
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-13 15:22:36 +0530

    Add bookAppointment component

--- src/components/book-appointment-form/BookAppointmentForm.js
+++ src/components/book-appointment-form/BookAppointmentForm.js
@@ -1,15 +1,36 @@
 import React, { useState } from 'react'
+import 'react-datepicker/dist/react-datepicker.css'
+import { DatePickerComponent } from '../date-picker/DatePickerComponent'
+import Axios from 'axios'
+import { useSelector } from 'react-redux'
+import Dropdown from '../dropdown/Dropdown'
+import request from '../../axios/request'
 
-const BookAppointmentForm = () => {
-  const [isActive, setIsActive] = useState(false)
+const BookAppointmentForm = ({facilityId, facilityName }) => {
+  const username = useSelector(state => state.user.currentUser);
+  const [isActive, setIsActive] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null)
+  const [city, setCity] = useState('')
+  const [formSubmitMessage, setFormSubmitMessage] = useState(null)
+  let dateToString;
 
+  if (selectedDate) {
+    const date = selectedDate.toString()
+    dateToString = `${date.split(' ').slice(0, 5).join(' ')} AM`
+  }
+
+ 
   return (
     <div>
+      {formSubmitMessage ? <h2>{formSubmitMessage}</h2> : null}
       {!isActive ?
         <button onClick={() => setIsActive(!isActive)}>BOOK A CLASS</button> :
-        <form>
-          <input type='date'/>
-          <button>BOOK NOW</button>
+        <form onSubmit={handleSubmit}>
+          <h2>NAME:- <span>{username}</span></h2>
+          <h2>Facility:- <span>{facilityName}</span></h2>
+          <DatePickerComponent selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+          <Dropdown setCity={setCity} />
+          <button type="submit">BOOK NOW</button>
         </form>
       }
     </div>
