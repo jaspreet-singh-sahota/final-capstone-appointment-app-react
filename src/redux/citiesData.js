4fb41509b95d0196f8d67c6ba562e24b7f796dfc
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-14 14:55:21 +0530

    Modify city data in redux store

---
+++ src/redux/citiesData.js
@@ -0,0 +1,12 @@
+export const cities = [
+  {id: 1, value: 'Delhi', label: 'Delhi'},
+  {id: 2, value: 'Mumbai', label: 'Mumbai'},
+  {id: 3, value: 'Bangalore', label: 'Bangalore'},
+  {id: 4, value: 'Hyderabad', label: 'Hyderabad'},
+  {id: 5, value: 'Ahmedabad', label: 'Ahmedabad'},
+  {id: 6, value: 'Chennai', label: 'Chennai'},
+  {id: 7, value: 'Kolkata', label: 'Kolkata'},
+  {id: 8, value: 'Surat', label: 'Surat'},
+  {id: 9, value: 'Jaipur', label: 'Jaipur'},
+  {id: 10, value: 'Lucknow', label: 'Lucknow'},
+]
