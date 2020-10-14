9b3e17cc4ff6e03db6a4e1f2e94a1946b21d6981
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-14 14:48:44 +0530

    Modify placeholder styles in dropdown component

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,17 +1,7 @@
 import React from 'react'
-import Select from 'react-dropdown-select'
 import { cities } from '../../redux/citiesData'
-import styles from "./styles/dropdown.css";
-import styled from 'styled-components/macro'
-
-const StyledSelect = styled(Select)`
- color: white !important;
-    border: 1px solid white !important;
-    border-radius: 20px !important;
-    max-width: 180px;
-    min-width: 150px
-`
-
+import { StyledSelect } from './styles/dropdown';
+import './styles/dropdown.css'
 
 const Dropdown = ({setCity}) => {
   const onChange = e => {
