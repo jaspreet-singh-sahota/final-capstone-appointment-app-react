f0edb3ac7ac008456bffc0698a57cba53819979b
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-15 22:56:07 +0530

    Add sign-in button

---
+++ src/components/log-out/LogOut.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { logOut } from '../../redux/actions/user-action/userAction'
+import Axios from 'axios'
+import request from '../../axios/request'
+import { NavLink } from 'react-router-dom'
+
+const LogOut = ({ setIsFacilityClicked }) => {
+  const isLoggedIn = useSelector(state => state.user.isLoggedIn)
+
+  const dispatch = useDispatch()
+  const handleLogOut = () => {
+    Axios
+      .delete(request.logUserOut, { withCredentials: true })
+      .then(response => {
+        if (response.data.logged_out) {
+          dispatch(logOut())
+        }
+      })
+      .catch(error => {
+        console.log("logout error", error);
+      });
+  }
+  return (
+    <div>
+      {isLoggedIn ? <h3 onClick={() => handleLogOut()}>Log Out</h3> :
+        <div>
+
+          <h3>
+            <NavLink onClick={() => setIsFacilityClicked(true)} to='/login' exact
+              activeStyle={{
+                backgroundColor: '#97bf0f',
+                color: 'white',
+                padding: '10px 69.9% 10px 10px',
+                marginLeft: '-10px'
+              }}
+            >
+              Log in
+          </NavLink>
+          </h3>
+          <h3 style={{marginTop: '20px'}}>
+            <NavLink onClick={() => setIsFacilityClicked(true)} to='/sign-in' exact
+              activeStyle={{
+                backgroundColor: '#97bf0f',
+                color: 'white',
+                padding: '10px 66.8% 10px 10px',
+                marginLeft: '-10px',
+              }}
+            >
+              Sign in
+          </NavLink>
+          </h3>
+        </div>
+      }
+    </div>
+  )
+}
+
+export default LogOut
