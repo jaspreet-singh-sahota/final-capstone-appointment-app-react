d32403684e56acb16ebc31fe8efe1e4ac53a04df
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-15 23:04:54 +0530

    Add appointment-reducer

---
+++ src/redux/reducers/appointment/appointmentReducer.js
@@ -0,0 +1,15 @@
+import { FETCH_APPOINTMENTS } from "../../actions/actionType";
+
+const INITIAL_STATE = {appointments: []};
+
+const appointmentReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case FETCH_APPOINTMENTS : return {
+      ...state,
+      appointments: action.payload
+    };
+    default: return state;
+  }
+};
+
+export default appointmentReducer;
