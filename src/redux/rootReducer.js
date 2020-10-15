df938f51be67aa83800a85bd669a2cb272f27d27
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-15 23:05:32 +0530

    Update root reducer

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+import userReducer from './reducers/user/userReducer'
+import facilityReducer from './reducers/facility/facilityReducer';
+import appointmentReducer from './reducers/appointment/appointmentReducer';
+
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  facility: facilityReducer,
+  appointment: appointmentReducer
+});
+
+export default rootReducer;
