607ffe8109b2fff4ecacad1ed73de93eaeea0aa9
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-15 22:42:15 +0530

    Remove unnecessary file

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './rootReducer';
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
