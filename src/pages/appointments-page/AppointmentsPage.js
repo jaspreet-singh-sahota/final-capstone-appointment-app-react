e5d97c07d3b68e6c74db818d1085c722623b3ded
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-16 18:50:04 +0530

    Add restrictions to appointments page

--- src/pages/appointments-page/AppointmentsPage.js
+++ src/pages/appointments-page/AppointmentsPage.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { Redirect } from 'react-router'
+import AppointmentsTable from '../../components/appointments-tabel/AppointmentsTable'
 
 const AppointmentsPage = () => {
+  const currentUser = useSelector(state => state.user.currentUser)
+
   return (
     <>
+      {currentUser ? <AppointmentsTable /> : <Redirect to='/login'/>}
     </>
   )
 }
