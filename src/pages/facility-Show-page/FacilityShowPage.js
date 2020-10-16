edc88ba4736b44cb0d2c43a10e3ef480d25a78ab
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-16 18:53:36 +0530

    Add restrictions to facility show page

--- src/pages/facility-Show-page/FacilityShowPage.js
+++ src/pages/facility-Show-page/FacilityShowPage.js
@@ -1,25 +1,36 @@
 import React from 'react'
 import BookAppointmentForm from '../../components/book-appointment-form/BookAppointmentForm'
 import styles from './styles/FacilityShowPage.module.css'
+import { Redirect } from 'react-router'
+import { useSelector } from 'react-redux'
 
-const FacilityShowPage = ({ location }) => {
-  const facility = location.state.facility
-
+const FacilityShowPage = ({ facilityData }) => {
+  const currentUser = useSelector(state => state.user.currentUser)
+  let facility;
+  
+  if (facilityData) {
+    facility = facilityData
+  }
+  
   return (
-    <div>
-      <img className={styles.image} alt='facility text' src={facility.image_url} />
-      <div className={styles['info-container']}>
-        <h1 className={styles.title}>{facility.name}</h1>
-        <h4 className={styles['bg-grey']}>Free Trial <div className={styles.price}>{facility.free_trial}</div></h4>
-        <h4>3 Months Package <div className={styles.price}>{facility.three_months_package}</div></h4>
-        <h4 className={styles['bg-grey']}>6 Months Package <div className={styles.price}>{facility.six_months_package}</div></h4>
-        <h4>Annual Package <div className={styles.price}>{facility.annual_package}</div></h4>
-        <h5 className={styles.detailed_description}>{facility.detailed_description}</h5>
-      </div>
-        <div className={styles.button}>
-          <BookAppointmentForm  facilityId={facility.id} facilityName={facility.name} />
+    <>
+      { currentUser ?
+        <div>
+          <img className={styles.image} alt='facility text' src={facility.image_url} />
+          <div className={styles['info-container']}>
+            <h1 className={styles.title}>{facility.name}</h1>
+            <h4 className={styles['bg-grey']}>Free Trial <div className={styles.price}>{facility.free_trial}</div></h4>
+            <h4>3 Months Package <div className={styles.price}>{facility.three_months_package}</div></h4>
+            <h4 className={styles['bg-grey']}>6 Months Package <div className={styles.price}>{facility.six_months_package}</div></h4>
+            <h4>Annual Package <div className={styles.price}>{facility.annual_package}</div></h4>
+            <h5 className={styles.detailed_description}>{facility.detailed_description}</h5>
+          </div>
+          <div className={styles.button}>
+            <BookAppointmentForm facilityId={facility.id} facilityName={facility.name} />
+          </div>
         </div>
-    </div>
+      : <Redirect to="/login" />}
+    </>
   )
 }
 
