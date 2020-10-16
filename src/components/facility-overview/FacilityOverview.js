097417df9f6600ed86f56d2aba8328ddca4298c2
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-16 18:38:01 +0530

    Modify styles of facility overview

--- src/components/facility-overview/FacilityOverview.js
+++ src/components/facility-overview/FacilityOverview.js
@@ -4,7 +4,7 @@ import { FaInstagram, FaFacebookF, FaTwitter } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import styles from './styles/facilityOverview.module.css'
 
-const FacilityOverview = ({ setIsFacilityClicked}) => {
+const FacilityOverview = ({setFacility}) => {
   const facilities = useSelector(state => state.facility.facilityCollection)
 
   return (
@@ -12,16 +12,16 @@ const FacilityOverview = ({ setIsFacilityClicked}) => {
       {facilities.map((facility, idx) => <div key={idx}>
         <div style={{position: 'relative'}}>
           <h3 className={styles['image-title']}>{facility.name}</h3>
-          <Link onClick={() => setIsFacilityClicked(true)} to={{ pathname: `/facility/:${idx}`, state: { facility } }} >
+          <Link onClick={() => setFacility(facility)} to={`/facility/${idx}`} >
             <img className={styles.image} alt='facility text' src={facility.image_url} />
             <h3 className={styles.title}>{facility.name}</h3>
             <h4 className={styles.info}>{facility.brief_description}</h4>
+          </Link>
             <div className={styles.icons}>
               <FaFacebookF style={{ fill: "#d1cfd0" }} className={styles.icon} />
               <FaTwitter style={{ fill: "#d1cfd0" }} className={styles.icon} />
               <FaInstagram style={{ fill: "#d1cfd0" }} className={styles.icon} />
             </div>
-          </Link>
         </div>
       </div>
       )}
