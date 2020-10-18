8ec31f957068e274d53cc7656f98a0e3569009eb
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-19 01:56:27 +0530

    Update proxy link

--- setupProxy.js
+++ setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://jassi-cultfit-app.herokuapp.com/',
+      target: 'https://jassi-appointment-app.herokuapp.com/',
       changeOrigin: true,
     })
   );
