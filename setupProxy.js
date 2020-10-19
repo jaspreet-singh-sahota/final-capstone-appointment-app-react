8ccd36d0da37ec248a24ece4040fa964fd2bf22a
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-19 10:45:01 +0530

    Modify proxy setup with target links

--- setupProxy.js
+++ setupProxy.js
@@ -2,7 +2,22 @@ var express = require('express');
 // Import the library:
 var cors = require('cors');
 
-var app = express();
+var whitelist = ['https://jassi-appointment-app.herokuapp.com/api/v1/logged_in',
+ 'https://jassi-appointment-app.herokuapp.com/api/v1/sessions',
+  'https://jassi-appointment-app.herokuapp.com/api/v1/logout',
+  'https://jassi-appointment-app.herokuapp.com/api/v1/registrations',
+  'https://jassi-appointment-app.herokuapp.com/api/v1/facilities',
+  'https://jassi-appointment-app.herokuapp.com/api/v1/set_appointment',
+  'https://jassi-appointment-app.herokuapp.com/api/v1/get_appointment',]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
-// Then use it before your routes are set up:
-app.use(cors());
+// Then pass them to cors:
+app.use(cors(corsOptions));
