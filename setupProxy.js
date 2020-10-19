70a18cbfd79235857999800ffc676f8acdc9630f
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-19 11:03:22 +0530

    Modify proxy setup with target links

--- setupProxy.js
+++ setupProxy.js
@@ -1,23 +1,25 @@
-var express = require('express');
-// Import the library:
-var cors = require('cors');
+const express = require('express');
+const request = require('request');
 
-var whitelist = ['https://jassi-appointment-app.herokuapp.com/api/v1/logged_in',
- 'https://jassi-appointment-app.herokuapp.com/api/v1/sessions',
-  'https://jassi-appointment-app.herokuapp.com/api/v1/logout',
-  'https://jassi-appointment-app.herokuapp.com/api/v1/registrations',
-  'https://jassi-appointment-app.herokuapp.com/api/v1/facilities',
-  'https://jassi-appointment-app.herokuapp.com/api/v1/set_appointment',
-  'https://jassi-appointment-app.herokuapp.com/api/v1/get_appointment',]
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('api/v1/logged_in', (req, res) => {
+  request(
+    { url: 'https://jassi-appointment-app.herokuapp.com/api/v1/logged_in' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
     }
-  }
-}
+  )
+});
 
-// Then pass them to cors:
-app.use(cors(corsOptions));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
