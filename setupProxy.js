bb12e6b1649ccde16b8dbfb61c78324b5c02501c
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-19 09:58:44 +0530

    Add proxy setup

--- setupProxy.js
+++ setupProxy.js
@@ -1,7 +1,7 @@
-// const express = require('express');
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// const app = express();
+const app = express();
 
-// app.use('/api', createProxyMiddleware({ target: 'https://jassi-appointment-app.herokuapp.com/api/v1/', changeOrigin: true }));
-// app.listen(3000);
+app.use('/api', createProxyMiddleware({ target: 'https://jassi-appointment-app.herokuapp.com/api/v1/', changeOrigin: true }));
+
