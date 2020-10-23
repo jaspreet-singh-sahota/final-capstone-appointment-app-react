432ec747812337d0ae896cf752b154ed3b247d5c
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-23 19:46:44 +0530

    Add async and await to axios requests with try and catch

--- src/axios/request.js
+++ src/axios/request.js
@@ -14,18 +14,19 @@ const request = {
   getAppointment: 'http://localhost:8080/api/v1/appointments',
 };
 
-export const requestSignUserIn = (dispatch, username, email, password,
-  passwordConfirmation, setErrors) => Axios.post(
-  request.SignUserIn,
-  {
-    user: {
-      username,
-      email,
-      password,
-      password_confirmation: passwordConfirmation,
-    },
-  },
-).then(response => {
+export const requestSignUserIn = async (dispatch, username, email, password,
+  passwordConfirmation, setErrors) => {
+  try {
+    const response = await Axios.post(request.SignUserIn,
+      {
+        user: {
+          username,
+          email,
+          password,
+          password_confirmation: passwordConfirmation,
+        },
+      }
+    )
     if (response.status === 201) {
       localStorage.setItem('token',
         JSON.stringify({
@@ -34,79 +35,91 @@ export const requestSignUserIn = (dispatch, username, email, password,
         }));
       dispatch(logIn(response.data.user.username));
     }
-  })
-  .catch(error => {
-    setErrors([error.response.data.errors])
-  });
+  } catch (error) {
+    setErrors([error.response.data.errors]);
+  };
+}
 
-export const requestAppointments = (dispatch, username) => Axios.get(request.getAppointment,
-  {
-    params: {
-      username,
-    },
-  })
-  .then(response => {
+export const requestAppointments = async (dispatch, username) => {
+  try {
+    const response = await Axios.get(request.getAppointment,
+      {
+        params: {
+          username,
+        },
+      }
+    );
     if (response.status === 200) {
       dispatch(fetchAppointments(response.data.appointments));
     }
-  })
-  .catch(error => {
+  }
+  catch (error) {
     fetchAppointments(error);
-  });
+  };
+}
 
-export const requestLogUserIn = (dispatch, username, password, setErrors) => Axios.post(
-  request.logUserIn,
-  {
-    user: {
-      username,
-      password,
-    },
-  },
-).then(response => {
-  if (response.status === 200) {
-    localStorage.setItem('token',
-    JSON.stringify({
-      key: response.data.token,
-      username: response.data.user.username,
-    }));
-    dispatch(logIn(response.data.user.username));
+export const requestLogUserIn = async (dispatch, username, password, setErrors) => {
+  try {
+    const request = await Axios.post(
+      request.logUserIn,
+      {
+        user: {
+          username,
+          password,
+        },
+      }
+    )
+    if (response.status === 200) {
+      localStorage.setItem('token',
+        JSON.stringify({
+          key: response.data.token,
+          username: response.data.user.username,
+        }));
+      dispatch(logIn(response.data.user.username));
+    }
   }
-})
-.catch(error => {
-  setErrors([error.response.data.error])
-});
+  catch (error) {
+    setErrors([error.response.data.error]);
+  };
+}
 
-export const requestFacilityData = dispatch => Axios.get(request.facilityData).then(response => {
-  if (response.status === 200) {
-    dispatch(fetchFacilitySuccess(response.data));
-  }
-}).catch(error => {
-  dispatch(fetchFacilityFailure(error.response.data.error));
-});
+export const requestFacilityData = async (dispatch) => {
+  try {
+    const response = await Axios.get(request.facilityData)
+    if (response.status === 200) {
+      dispatch(fetchFacilitySuccess(response.data));
+    }
+  } catch (error) {
+    dispatch(fetchFacilityFailure(error.response.data.error));
+  };
+};
 
-export const bookAppointment = (facilityId, dateToString, city, username,
-  setIsActive, setFormSubmitMessage, config) => Axios.post(
-  request.setAppointment,
-  {
-    config,
-    appointment: {
-      facility_id: facilityId,
-      date: dateToString,
-      city,
-      username,
-    },
-  },
-).then(response => {
-  if (response.status === 201) {
-    setIsActive(false);
-    setFormSubmitMessage(`You successfully booked an appointment on ${dateToString}`);
-    setTimeout(() => {
-      setFormSubmitMessage(null);
-    }, 5000);
-  }
-}).catch(error => {
+export const bookAppointment = async (facilityId, dateToString, city, username,
+  setIsActive, setFormSubmitMessage, config) => {
+  try {
+    const response = await Axios.post(
+      request.setAppointment,
+      {
+        config,
+        appointment: {
+          facility_id: facilityId,
+          date: dateToString,
+          city,
+          username,
+        },
+      },
+    )
+    if (response.status === 201) {
+      setIsActive(false);
+      setFormSubmitMessage(`You successfully booked an appointment on ${dateToString}`);
+      setTimeout(() => {
+        setFormSubmitMessage(null);
+      }, 5000);
+    }
+  } catch (error) {
     setFormSubmitMessage(`There was ${error.response.data.error} while booking the appointment. Try again after a while`);
     setTimeout(() => {
       setFormSubmitMessage(null);
     }, 5000);
-  });
+  };
+}
